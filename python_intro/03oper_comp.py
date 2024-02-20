85c3c51778d6b1457a74bfdc6290690771dcc637
Gustavocaris <149839159+Gustavocaris@users.noreply.github.com>
2024-02-19 21:25:01 -0300

    Adicionando alguns conceitos básicos do Python

---
+++ python_intro/03oper_comp.py
@@ -0,0 +1,17 @@
+#           == igual 
+#           != diferente 
+#           <= menor ou igual que
+#           >= maior ou igual que
+
+x = y = z = False
+n1 = n2 = 0
+
+print("Digite um numero: ")
+n1 = int(input())
+n2 = int(input('Digite outro número: '))
+
+x = n1 == 2
+print(n1, 'é maior que', n2, '?', z, '\n')
+
+y = n1 != n2
+print('são diferentes?' + str(y))
---
+++ python_intro/08for.py
@@ -0,0 +1,16 @@
+# for item in sequencia: 
+#       intruções executadas para cada item da sequencia
+#       ou seja, um valor -> uma sequencia especifica pra ele
+
+
+#----------------------EXEMPLO------------------
+# lista = [2, 3, 4, 7, 1, 9, 10, 25, 32, 72, 23]
+
+# for item in lista:
+#     print(item)
+
+#--------------------EXEMPLO-----------------
+lista = [2, 3, 4, 7, 1, 9, 10, 25, 32, 72, 23]
+palavra = 'Gustavo'
+for letra in palavra: # Perceba -> para cada letra uma palavra
+    print(letra)

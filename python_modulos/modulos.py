367814fb8292448db33e04e665a3ffd3a4bedf7b
Gustavocaris <149839159+Gustavocaris@users.noreply.github.com>
2024-02-20 22:27:06 -0300

    modulos em py

---
+++ python_modulos/modulos.py
@@ -0,0 +1,8 @@
+# eu to dizendo pra importar do modulo math somente a funcionalidade para calcular raiz quadrada
+from math import sqrt
+
+# módulo é um arquivo contendo definições e declarações de Python. 
+# O nome do arquivo é o nome do módulo com a extensão .py. Um módulo pode definir funções, 
+# classes e variáveis, além de executar código quando é importado
+
+# Para usar funcionalidades de um módulo em seu programa, você precisa importá-lo usando a instrução import.

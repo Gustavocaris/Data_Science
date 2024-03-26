d40ccaa7da349273a737a18bd8e10aa807e1a86d
gustavo caris <149839159+Gustavocaris@users.noreply.github.com>
2024-03-26 12:53:53 -0300

    desafio de comparar duas palavras dentro de uma função e verificar se realmente é um anagrama

--- 02_python_intro/Desafios_treino/01_anagramas.py
+++ 02_python_intro/Desafios_treino/01_anagramas.py
@@ -3,6 +3,34 @@
 # Por exemplo, "listen" é um anagrama de "silent". Neste exercício, você escreverá uma função em 
 # Python para verificar se duas palavras são anagramas uma da outra.
 
-# Desafio:
+# DESAFIO:
 # Escreva uma função chamada sao_anagramas(palavra1, palavra2) que recebe duas strings como entrada 
 # e retorna True se as duas palavras são anagramas uma da outra e False caso contrário.
+
+
+
+def anagramas(palavra1, palavra2):
+    palavra1 = palavra1.lower()
+    palavra2 = palavra2.lower()
+
+    if len(palavra1) != len(palavra2):  # 'len' é para contar o tamanho da palavra;
+       return False
+
+    contagem1 = {}
+    contagem2 = {}
+
+    for letra in palavra1:
+        contagem1[letra] = contagem1.get(letra, 0) + 1
+
+    for letra in palavra2:
+        contagem2[letra] = contagem2.get(letra, 0) + 1
+
+    return contagem1 == contagem2
+
+# Solicita ao usuário que insira as palavras
+palavra1 = input("Digite a primeira palavra: ")
+palavra2 = input("Digite a segunda palavra: ")
+
+# Chama a função sao_anagramas com as palavras fornecidas pelo usuário e imprime o resultado
+print(anagramas(palavra1, palavra2))
+

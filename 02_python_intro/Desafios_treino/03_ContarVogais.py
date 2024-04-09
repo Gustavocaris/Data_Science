c2528239c48c988c3c8c6e032f35c4a2d3f59c85
gustavo caris <149839159+Gustavocaris@users.noreply.github.com>
2024-04-09 20:04:24 -0300

    Iniciando novo exercicio em PY

--- 02_python_intro/Desafios_treino/03_ContarVogais.py
+++
@@ -1,6 +0,0 @@
-# ------> Descrição do Problema:
-# Dada uma string, você precisa contar quantas vogais (letras 'a', 'e', 'i', 'o', 'u') estão presentes nela.
-# Seu desafio é escrever uma função em Python chamada contar_vogais que recebe uma string como entrada e retorna o número de vogais presentes na string.
-
-# EXEMPLO
-# print(contar_vogais("Python"))  # Saída esperada: 1 (porque 'o' é uma vogal)
---
+++ 02_python_intro/Desafios_treino/03_FatorialNum.py
@@ -0,0 +1,7 @@
+# exercicio
+
+# Escreva um programa Python que calcula o fatorial de um número inteiro não negativo dado pelo usuário.
+
+def calcular_fatorial(numero):
+    if numero == 0 or numero == 1:
+        return 1

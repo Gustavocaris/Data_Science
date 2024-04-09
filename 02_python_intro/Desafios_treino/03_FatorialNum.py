163e8066dee48cfc7a99ddf037e50d772edd1471
gustavo caris <149839159+Gustavocaris@users.noreply.github.com>
2024-04-09 20:20:20 -0300

    Resolução final com auxilio da IA

--- 02_python_intro/Desafios_treino/03_FatorialNum.py
+++ 02_python_intro/Desafios_treino/03_FatorialNum.py
@@ -5,3 +5,17 @@
 def calcular_fatorial(numero):
     if numero == 0 or numero == 1:
         return 1
+    else:
+        fatorial = 1
+        conta = ' x '.join(str(i) for i in range(2, numero + 1))
+        for i in range(2, numero + 1):
+            fatorial *= i
+        return fatorial, conta
+    
+numero = int(input('Digite um numero aqui inteiro e não negativo:  '))
+
+if numero < 0:
+    print('Digite um numero inteiro e positivo!!')
+else :
+    resultado = calcular_fatorial(numero)
+    print(f"O fatorial de {numero} é {resultado}.")

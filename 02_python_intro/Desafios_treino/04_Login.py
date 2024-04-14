7ebe77bfd420ea57326e9f281527981092186e25
Gustavocaris <149839159+Gustavocaris@users.noreply.github.com>
2024-04-13 21:40:54 -0300

    Criando uma tela de Login simples pra entender essa biblioteca

--- 02_python_intro/Desafios_treino/04_Login.py
+++ 02_python_intro/Desafios_treino/04_Login.py
@@ -1,7 +1,26 @@
-from PySimpleGUI import pySimpleGUI as sg
+from PySimpleGUI import PySimpleGUI as sg
 
-# Lay
+# Layout
+
+# ---> vamos pensar em linhas e coluna
+sg.theme('Reddit')
+layout = [
+    [sg.Text('Usuário'), sg.Input(key='usuario', size=(20,1))],
+    [sg.Text('Senha'), sg.Input(key='senha', password_char='*', size=(20,1))],
+    [sg.Checkbox('Salvar o Login? ')],
+    [sg.Button('Entrar')]
+
+]
 
 # Janela 
+janela = sg.Window('Tela de Login', layout)
 
 # Ler eventos
+
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores['usuario'] == 'Gustavo' and valores['senha'] == '123456':
+            print('Bem-Vindo, meu senhor')

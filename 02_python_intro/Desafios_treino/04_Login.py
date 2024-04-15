812a15e2475428a1465bfe0b01af0b9605710eca
gustavo caris <149839159+Gustavocaris@users.noreply.github.com>
2024-04-15 20:09:31 -0300

    Aprendendo + sobre o PySimpleGUI trabalhando com telas

--- 02_python_intro/Desafios_treino/04_Login.py
+++ 02_python_intro/Desafios_treino/04_Login.py
@@ -3,12 +3,29 @@ from PySimpleGUI import PySimpleGUI as sg
 # Layout
 
 # ---> vamos pensar em linhas e coluna
-sg.theme('Reddit')
+sg.theme('Dark Blue 3')
 layout = [
-    [sg.Text('Usuário'), sg.Input(key='usuario', size=(20,1))],
-    [sg.Text('Senha'), sg.Input(key='senha', password_char='*', size=(20,1))],
+    [sg.Text('Nome Completo'), sg.Input(key='nome_completo', password_char='*', size=(40,2))],
+    [sg.Text('Data de Nascimento'), sg.Input(key='data_nascimento', size=(10,1)), sg.Text('Idade'), sg.Input(key='idade', size=(5,1))],
+    [sg.Text('Idade'), sg.Input(key='idade', password_char='*', size=(40,2))],
+    [sg.Text('')],###############################################################################
+    [sg.Text('Usuário'), sg.Input(key='usuario', size=(40,2))],
+    [sg.Text('Senha'), sg.Input(key='senha', password_char='*', size=(40,2))],
     [sg.Checkbox('Salvar o Login? ')],
-    [sg.Button('Entrar')]
+    [sg.Text('')],###############################################################################
+    [sg.Button('Entrar' , size=(10, 1), pad=((150, 0), 3),button_color=('white', 'red'))],
+    [sg.Listbox(values=['Opção 1', 'Opção 2', 'Opção 3'], key='chave_da_lista', size=(40,5 ))],
+    [sg.FileBrowse(key='chave_do_seletor_de_arquivo')],
+    [sg.FolderBrowse(key='chave_do_seletor_de_pasta')],
+    [sg.CalendarButton('Selecione a data', key='chave_do_seletor_de_data')],
+    [sg.Combo(values=['Opção 1', 'Opção 2', 'Opção 3'], key='chave_do_combobox', size=(20, 1))],
+    [sg.Radio('Opção 1', 'opcoes', key='opcao_1'), sg.Radio('Opção 2', 'opcoes', key='opcao_2')],
+    [sg.Button('Confirmar')]
+    
+
+    
+
+    
 
 ]
 
@@ -24,3 +41,5 @@ while True:
     if eventos == 'Entrar':
         if valores['usuario'] == 'Gustavo' and valores['senha'] == '123456':
             print('Bem-Vindo, meu senhor')
+
+janela.close()

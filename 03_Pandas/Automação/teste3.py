d0052d25fa26ee6c7885ac2c356443c81a4f4813
gustavo caris <149839159+Gustavocaris@users.noreply.github.com>
2024-10-22 19:47:58 -0300

    estruturando automation com base teste

--- 03_Pandas/Automação/teste3.py
+++ 03_Pandas/Automação/teste3.py
@@ -1,9 +1,21 @@
 import pyautogui 
 import time
 
-print(pyautogui.position())
-print(pyautogui.size())
+
+time.sleep(5)
+
+pyautogui.click(1186,309, duration=2)
+
+pyautogui.click(584,331, duration=2)
+
+with open('CustomersTest.txt', 'r') as arquivo:
+    for linha in arquivo:
+        nome = linha.split(',')[0]
+        email = linha.split(',')[1]
+        contato = linha.split(',')[2]
+
+
 
 # function mouse
-time.sleep(5)
+
 #pyautogui.click(x=684, y=557)
